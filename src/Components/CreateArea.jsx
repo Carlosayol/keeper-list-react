544adfc17828aecd8405b8a13d1ab0919270e4f2
carlosayol <carlosaolar@gmail.com>
2021-05-22 17:08:56 -0500

    changing pc

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+    const [noteData, setnoteData] = useState({
+        title: "",
+        content: ""
+    });
+
+
+    return (
+        <div>
+        <form>
+            <input name="title" placeholder="Title" />
+            <textarea name="content" placeholder="Take a note..." rows="3" />
+            <button>Add</button>
+        </form>
+        </div>
+    );
 }
 
 export default CreateArea;
